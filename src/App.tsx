c18a1e1b963f09a781724455aaa37207635aab33
kakakakakku <y.yoshida22@gmail.com>
2019-10-12 15:56:23 +0900

    Added Snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import Snackbar from '@material-ui/core/Snackbar';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    close: {
+      padding: theme.spacing(1.5),
+    },
+  }),
+);
+
 const App: React.FC = () => {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
@@ -27,6 +40,18 @@ const App: React.FC = () => {
         autoHideDuration={5000}
         onClose={handleClose}
         message={<span id="message-id">This is Snackbar 🎃</span>}
+        action={[
+          <Button color="secondary" size="medium" onClick={handleClose}>取り消し</Button>,
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            className={classes.close}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
       />
     </div>
   );
